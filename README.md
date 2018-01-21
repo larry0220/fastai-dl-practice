d7c175ad01f2195616d1af3e94e805ebda76d645
larry <larry@larryde-Mac-mini.local>
2018-01-21 23:14:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# fastai-dl-practice
+
+[Fastai deep learning](http://course.fast.ai/) (Part 1 v1) 的學習筆記
+
+## 前置作業
+
+1. 安裝 [Miniconda (Python 3.6)](https://conda.io/miniconda.html)
+
+1. Install 以下 library
+  - `conda install matplotlib jupyter scikit-learn bcolz pillow h5py -y`
+
+  - `pip install keras`
+
+  - tensorflow 擇一安裝，沒有 GPU: `pip install tensorflow`，有 GPU:
+`pip install tensorflow-gpu`
+
+1. 下載 [dogscats.zip](http://files.fast.ai/data/dogscats.zip) 在 `course/data` 解壓縮
+
+```
+├── data
+│   └── dogscats
+├── lesson1.ipynb
+├── utils.py
+└── vgg16.py
+```
+
+## 參考資料
+
+- [fast-ai-part1-v1](http://wiki.fast.ai/index.php/Main_Page)
+
+- [fast-ai-part1-v2](http://forums.fast.ai/t/welcome-to-part-1-v2/5787)
+
+- [[線上讀書會] 線上深度機器學習 讀書會 C組 第五次](https://www.youtube.com/watch?v=oOWwzOHp6Lo)
+
+- [政治大學應用數學系《數學軟體應用》課程的上課筆記](https://github.com/yenlung/nccu-jupyter-math)
+
+- [用功的資料工程師也可以學的會的深度學習](https://github.com/erhwenkuo/deep-learning-with-keras-notebooks)
---
+++ course/utils.py
@@ -0,0 +1,98 @@
+import math, os, json, sys, re
+import numpy as np
+from matplotlib import pyplot as plt
+import itertools
+
+from numpy.random import random, permutation, randn, normal, uniform, choice
+from numpy import newaxis
+import scipy
+# from sklearn.metrics import confusion_matrix
+import bcolz
+from sklearn.preprocessing import OneHotEncoder
+
+from keras.utils import to_categorical
+from keras.preprocessing import image
+from keras.utils.data_utils import get_file
+
+np.set_printoptions(precision=4, linewidth=100)
+
+
+def floor(x):
+    return int(math.floor(x))
+
+
+def ceil(x):
+    return int(math.ceil(x))
+
+
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, len(ims)//rows, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+        (This function is copied from the scikit docs.)
+    """
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print(cm)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+def get_batches(dirname, gen=image.ImageDataGenerator(), shuffle=True, batch_size=4, class_mode='categorical',
+                target_size=(224,224)):
+    return gen.flow_from_directory(dirname, target_size=target_size,
+            class_mode=class_mode, shuffle=shuffle, batch_size=batch_size)
+
+
+def get_data(path, target_size=(224,224)):
+    batches = get_batches(path, shuffle=False, batch_size=1, class_mode=None, target_size=target_size)
+    return np.concatenate([batches.next() for i in range(batches.samples)])
+
+
+def get_classes(path):
+    batches = get_batches(path+'train', shuffle=False, batch_size=1)
+    val_batches = get_batches(path+'valid', shuffle=False, batch_size=1)
+    test_batches = get_batches(path+'test', shuffle=False, batch_size=1)
+    return (val_batches.classes, batches.classes, onehot(val_batches.classes), onehot(batches.classes),
+        val_batches.filenames, batches.filenames, test_batches.filenames)
+
+
+def onehot(x):
+    return to_categorical(x)
+
+
+def save_array(fname, arr):
+    c=bcolz.carray(arr, rootdir=fname, mode='w')
+    c.flush()
+
+
+def load_array(fname):
+    return bcolz.open(fname)[:]
+
